0500a608eb3ceb66f2e7d0eb1ba6953e65e28ed2
Brenda de Paula <brnd.dpaula@gmail.com>
2024-06-22 15:14:14 -0300

    Create initclass

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String nome = "Brenda de Paula";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
+
+        System.out.println("***********************");
+        System.out.println("Seus dados:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo inicial: " + saldo);
+        System.out.println("***********************");
 
     }
-    }
+}
